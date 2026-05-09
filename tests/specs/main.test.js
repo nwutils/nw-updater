375b4a25b1949269298b452fc4d92e82f1fb7907
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2026-05-09 19:44:59 +0530

    chore(ci): build current and latest app for test suite

--- tests/specs/main.test.js
+++ tests/specs/main.test.js
@@ -1,4 +1,6 @@
 import assert from "node:assert/strict";
+import fs from "node:fs";
+import path from "node:path";
 import { before, describe, it } from "node:test";
 
 import nwbuild from "nw-builder";
@@ -7,8 +9,34 @@ import nwbuild from "nw-builder";
 
 describe("updater test suite", function () {
 
-    before(function () {
+    let nwOptions = {
+        mode: "build",
+        version: "latest",
+        flavor: "normal",
+        platform: "linux",
+        arch: "x64",
+        glob: false,
+        zip: "zip",
+    };
 
+    before(async function () {
+        nwOptions = {
+            ...nwOptions,
+            srcDir: "./tests/fixtures/app-current",
+            outDir: "./tests/fixtures/releases/app-0.0.1-linux-x64"
+        };
+        if (!fs.existsSync(`${nwOptions.outDir}.zip`)) {
+            await nwbuild(nwOptions);
+        }
+
+        nwOptions = {
+            ...nwOptions,
+            srcDir: "./tests/fixtures/app-latest",
+            outDir: "./tests/fixtures/releases/app-0.0.2-linux-x64"
+        };
+        if (!fs.existsSync(`${nwOptions.outDir}.zip`)) {
+            await nwbuild(nwOptions);
+        }
     });
 
     it("updates the application", async function () {
