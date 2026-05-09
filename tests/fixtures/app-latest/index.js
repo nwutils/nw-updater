457522794606fd53540e8cbdabefb42d13ea27be
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2026-05-09 22:41:49 +0530

    chore(test): setup testing environment

--- tests/fixtures/app-latest/index.js
+++ tests/fixtures/app-latest/index.js
@@ -1,1 +1,19 @@
-const App = nw.App;
+import Updater from './updater.js';
+
+const updater = new Updater("http://localhost:3000/releases/manifest.json");
+
+async function handleCheckForUpdates() {
+    const updateStatus = document.getElementById('update-status');
+    updater.checkNewVersion((err, newerVersionExists, remoteManifest) => {
+            updateStatus.textContent = "Checking for updates...";
+        if (err) {
+            updateStatus.textContent = `Error checking for updates: ${err.message}`;
+            return;
+        }
+        if (newerVersionExists) {
+            updateStatus.textContent = 'A newer version is available.';
+        } else {
+            updateStatus.textContent = 'No new version available.';
+        }
+    });
+}
--- tests/specs/main.test.js
+++ tests/specs/main.test.js
@@ -6,40 +6,62 @@ import { after, before, describe, it } from "node:test";
 
 import express from "express";
 import nwbuild from "nw-builder";
+import selenium from "selenium-webdriver";
+import chrome from "selenium-webdriver/chrome.js";
 
 // import get from "../../src/main.js";
 
 describe("updater test suite", function () {
 
+    let driver = undefined;
+
     let nwOptions = {
         mode: "build",
         version: "latest",
-        flavor: "normal",
+        flavor: "sdk",
         platform: "linux",
         arch: "x64",
         glob: false,
-        zip: "zip",
     };
 
     const app = express();
     const filesDir = path.join(process.cwd(), "tests", "fixtures", "releases");
-    app.use('/releases', express.static(filesDir));
+    app.use("/releases", express.static(filesDir));
     const server = http.createServer(app);
 
+    const options = new chrome.Options();
+    const seleniumArguments = [
+        "nwapp=" + path.resolve("tests", "fixtures", "app-current")
+    ];
+    seleniumArguments.push("headless=new");
+    options.addArguments(seleniumArguments);
+    const chromeDriverPath = path.resolve("cache", "nwjs-sdk-v0.111.1-linux-x64", "chromedriver");
+    const service = new chrome.ServiceBuilder(chromeDriverPath).build();
+    driver = chrome.Driver.createSession(options, service);
+
     before(async function () {
+
+        fs.copyFileSync("./src/main.js", "./tests/fixtures/app-current/updater.js");
+
         nwOptions = {
             ...nwOptions,
             srcDir: "./tests/fixtures/app-current",
             outDir: "./tests/fixtures/releases/app-0.0.1-linux-x64"
         };
-        if (!fs.existsSync(`${nwOptions.outDir}.zip`)) {
+        if (!fs.existsSync(nwOptions.outDir)) {
             await nwbuild(nwOptions);
         }
 
+        fs.cpSync("./tests/fixtures/app-current", "./tests/fixtures/app-latest", { recursive: true });
+        const latestPackageJsonPath = path.join(process.cwd(), "tests", "fixtures", "app-latest", "package.json");
+        const latestPackageJson = JSON.parse(fs.readFileSync(latestPackageJsonPath, "utf-8"));
+        latestPackageJson.version = "0.0.2";
+        fs.writeFileSync(latestPackageJsonPath, JSON.stringify(latestPackageJson, null, 2), "utf-8");
         nwOptions = {
             ...nwOptions,
             srcDir: "./tests/fixtures/app-latest",
-            outDir: "./tests/fixtures/releases/app-0.0.2-linux-x64"
+            outDir: "./tests/fixtures/releases/app-0.0.2-linux-x64",
+            zip: "zip",
         };
         if (!fs.existsSync(`${nwOptions.outDir}.zip`)) {
             await nwbuild(nwOptions);
@@ -50,8 +72,22 @@ describe("updater test suite", function () {
         });
     });
 
-    it("updates the application", async function () {
-        assert.strictEqual(1 === 1, true);
+    it("runs the current application and checks for updates", async function () {
+        const button = await driver.findElement(selenium.By.id('check-for-updates-button'));
+        await button.click();
+        const statusLocator = selenium.By.id('update-status');
+        const initialText = await driver.findElement(statusLocator).getText();
+
+        assert.strictEqual(initialText, "Checking for updates...");
+
+        await driver.wait(async () => {
+            const el = await driver.findElement(statusLocator);
+            const text = await el.getText();
+            return text !== initialText;
+        }, 10000);
+
+        const finalText = await driver.findElement(statusLocator).getText();
+        assert.strictEqual(finalText, "A newer version is available.");
     });
 
     after(async function () {
@@ -61,5 +97,6 @@ describe("updater test suite", function () {
                 else resolve();
             });
         });
+        await driver.quit();
     });
 });
